05925562d64f8792c6e9f86cf052c041153eef95
Stricks-dev <77883569+Stricks-dev@users.noreply.github.com>
2021-10-22 20:38:54 +0530

    1st iteration

---
+++ New Unity Project/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    public float gravity = 9f;
+    public float jumpSpeed = 15f;
+
+    [Space]
+    public CharacterController controller;
+
+    private void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        bool jump = Input.GetKeyDown(KeyCode.Space);
+
+        MovePlayer(input, jump);
+    }
+
+    Vector3 xzVelocity;
+    Vector3 yVelocity;
+    Vector3 playerFinalVel;
+
+    bool isOnGround;
+    private void MovePlayer(Vector2 input, bool jump)
+    {
+        xzVelocity = new Vector3(input.y, 0f, input.x) * moveSpeed * Time.deltaTime;
+        if (!controller.isGrounded)
+            yVelocity += Vector3.down * gravity * Time.deltaTime;
+        else
+            yVelocity = Vector3.zero;
+
+        if (jump && controller.isGrounded)
+            yVelocity += Vector3.up * jumpSpeed;
+
+        playerFinalVel = xzVelocity + yVelocity * Time.deltaTime;
+        controller.Move(playerFinalVel);
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        isOnGround = hit.gameObject.layer == LayerMask.NameToLayer("GroundObjects");
+    }
+}
