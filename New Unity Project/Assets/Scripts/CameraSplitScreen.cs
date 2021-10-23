b42c2158e94013a89588e7521397c2fe0bc7eb2e
Stricks-dev <77883569+Stricks-dev@users.noreply.github.com>
2021-10-23 10:19:30 +0530

    new splitscreen

--- New Unity Project/Assets/Scripts/CameraSplitScreen.cs
+++ New Unity Project/Assets/Scripts/CameraSplitScreen.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraSplitScreen : MonoBehaviour
 {
@@ -9,11 +10,17 @@ public class CameraSplitScreen : MonoBehaviour
     [SerializeField] Camera frontCam;
     [Space]
     [Range(0f, 1f)] public float sideCamThickness;
-
+    [Space]
+    [SerializeField] Image splitter;
     float frontCamThickness;
     float frontCamXDis;
 
     bool doSplit = false;
+    private void Start()
+    {
+        frontCamThickness = 1f - sideCamThickness;
+        frontCamXDis = sideCamThickness;
+    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.V))
@@ -25,6 +32,24 @@ public class CameraSplitScreen : MonoBehaviour
 
     private void HandleSplit()
     {
-        
+        if (doSplit)
+        {
+            //Enable splitting
+            frontCam.gameObject.SetActive(true);
+            sideCam.rect = new Rect(0f, 0f, sideCamThickness, 1f);
+            frontCam.rect = new Rect(frontCamXDis, 0f, frontCamThickness, 1f);
+
+            splitter.gameObject.SetActive(true);
+            float xPos = sideCamThickness * Screen.width;
+            Vector2 splitterPos = new Vector2(xPos, splitter.rectTransform.position.y);
+            splitter.rectTransform.position = splitterPos;
+        }
+        else
+        {
+            frontCam.gameObject.SetActive(false);
+            sideCam.rect = new Rect(0f, 0f, 1f, 1f);
+
+            splitter.gameObject.SetActive(false);
+        }
     }
 }
